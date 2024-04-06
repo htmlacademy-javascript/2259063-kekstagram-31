29bb27d3fc557c234d7bd43a5a5fa85ffde73422
dobroeytro <warm.citrus@gmail.com>
2024-04-06 17:28:56 +0300

    правки автотестов

--- js/form/form-effects-slider.js
+++ js/form/form-effects-slider.js
@@ -5,8 +5,8 @@ const effectFieldset = document.querySelectorAll('.img-upload__effects');
 const uploadPicturePreviev = document.querySelector('.img-upload__preview img');
 
 const effectsMap = {
-  chrome: { filter: 'grayscale', unit: '', min: 0, max: 10, step: 1 },
-  sepia: { filter: 'sepia', unit: '', min: 0, max: 10, step: 1 },
+  chrome: { filter: 'grayscale', unit: '', min: 0, max: 1, step: 0.1 },
+  sepia: { filter: 'sepia', unit: '', min: 0, max: 1, step: 0.1 },
   marvin: { filter: 'invert', unit: '%', min: 0, max: 100, step: 1 },
   phobos: { filter: 'blur', unit: 'px', min: 0, max: 3, step: 0.1 },
   heat: { filter: 'brightness', unit: '', min: 1, max: 3, step: 0.1 }
@@ -15,23 +15,23 @@ const effectsMap = {
 let currentEffect = 'none';
 const updatePictureStyle = () => {
   const effect = effectsMap[currentEffect];
-  let value;
+
+  let resultEffect;
 
   if (currentEffect === 'chrome' || currentEffect === 'sepia') {
-    value = effect.min + (effectLevelValue.value * (effect.max - effect.min) / 100);
-    uploadPicturePreviev.style.filter = `${effect.filter}(${value})`;
+    resultEffect = effect.min + (effectLevelValue.value * (effect.max - effect.min));
     effectLevelSliderContainer.classList.remove('hidden');
   } else if (currentEffect === 'marvin') {
-    value = effect.min + Math.round(effectLevelValue.value * (effect.max - effect.min) / 100);
-    uploadPicturePreviev.style.filter = `${effect.filter}(${value}${effect.unit})`;
+    resultEffect = effect.min + Math.round(effectLevelValue.value * (effect.max - effect.min) / 100);
+    uploadPicturePreviev.style.filter = `${effect.filter}(${resultEffect}${effect.unit})`;
     effectLevelSliderContainer.classList.remove('hidden');
   } else if (currentEffect === 'phobos') {
-    value = Math.round(effectLevelValue.value * (effect.max - effect.min) / 100);
-    uploadPicturePreviev.style.filter = `${effect.filter}(${value}${effect.unit})`;
+    resultEffect = effectLevelValue.value;
+    uploadPicturePreviev.style.filter = `${effect.filter}(${resultEffect}${effect.unit})`;
     effectLevelSliderContainer.classList.remove('hidden');
   } else if (currentEffect === 'heat') {
-    value = effect.min + (effectLevelValue.value * (effect.max - effect.min) / 100);
-    uploadPicturePreviev.style.filter = `${effect.filter}(${value})`;
+    resultEffect = effectLevelValue.value ;
+    uploadPicturePreviev.style.filter = `${effect.filter}(${resultEffect})`;
     effectLevelSliderContainer.classList.remove('hidden');
   } else {
     uploadPicturePreviev.style.filter = 'none';
@@ -50,6 +50,10 @@ const changeEffectHandler = (evt) => {
         range: {
           min: effect.min,
           max: effect.max
+        },
+        format : {
+          to: (value) => Number(value),
+          from: (value) => Number(value),
         }
       });
       effectLevelSlider.noUiSlider.set(effect.max);
@@ -58,7 +62,6 @@ const changeEffectHandler = (evt) => {
       effectLevelSlider.classList.add('hidden');
     }
 
-    updatePictureStyle();
     if (currentEffect === 'none') {
       effectLevelSlider.classList.add('hidden');
     } else {
@@ -99,8 +102,8 @@ function resetEffectSlider() {
   uploadPicturePreviev.style.filter = 'none';
   effectLevelSliderContainer.classList.add('hidden');
   currentEffect = 'none';
-  updatePictureStyle();
-  removeEffectFieldsetEventHandler();
+  // updatePictureStyle();
+  // removeEffectFieldsetEventHandler();
 }
 
 const deleteSliderOnNoneEffect = () => {
